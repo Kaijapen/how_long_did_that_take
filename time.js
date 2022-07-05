3f3976c4d1acb10ac0c8a29b26ae8941735d92c1
Kaijapen <104542328+Kaijapen@users.noreply.github.com>
2022-07-05 14:23:52 -0700

    idk

--- time.js
+++ time.js
@@ -13,7 +13,7 @@ const { performance } = require('perf_hooks');
 const start = performance.now();
 let primeCount = 0;
 let num = 2; // for math reasons, 1 is considered prime
-while( primeCount < 1e6 ) {
+while( primeCount < 1e4 ) {
     if( num.isPrime() ) {
         primeCount++;
     }
@@ -49,10 +49,10 @@ function iFib( n ) {
 console.log(iFib(20));
 
 // 3.
+const story = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident culpa nihil repellat nulla laboriosam maxime, quia aliquam ipsam reprehenderit delectus reiciendis molestias assumenda aut fugit tempore laudantium tempora aspernatur? Repellendus consequatur expedita doloribus soluta cupiditate quae fugit! Aliquid, repellat animi, illum molestias maiores, laboriosam vero impedit iusto mollitia optio labore asperiores!";
+
 const reversed1 = story.split("").reverse().join("");
 console.log(reversed1);
-
-const story = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident culpa nihil repellat nulla laboriosam maxime, quia aliquam ipsam reprehenderit delectus reiciendis molestias assumenda aut fugit tempore laudantium tempora aspernatur? Repellendus consequatur expedita doloribus soluta cupiditate quae fugit! Aliquid, repellat animi, illum molestias maiores, laboriosam vero impedit iusto mollitia optio labore asperiores!";
 
 // more efficient? maybe?
 function reverseString(string){
